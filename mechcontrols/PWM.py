da1fd71486d79b4ba13f76699fcf43c2c91ffb94
Alan <alyeldinshahin@gmail.com>
2024-08-17 03:11:23 -0400

    test add

--- mechcontrols/PWM.py
+++ mechcontrols/PWM.py
@@ -1,7 +1,13 @@
 from gpiozero import OutputDevice, PWMOutputDevice
 
 class MotorController:
-        power_pin_forward = OutputDevice(17, initial_value=False)  # Direction pin 1 for forward
-        power_pin_reverse = OutputDevice(27, initial_value=False)  # Direction pin 2 for reverse
-        pwm_power = PWMOutputDevice(18, frequency=1000, initial_value=0)  # PWM pin for power control
-        pwm_steering = PWMOutputDevice(12)  # PWM pin for steering control
+    power_pin_forward = OutputDevice(17, initial_value=False)  # Direction pin 1 for forward
+    power_pin_reverse = OutputDevice(27, initial_value=False)  # Direction pin 2 for reverse
+    pwm_power = PWMOutputDevice(18, frequency=1000, initial_value=0)  # PWM pin for power control
+    pwm_steering = PWMOutputDevice(12)  # PWM pin for steering control
+    
+    def __init__(self):
+        self.power_pin_forward = OutputDevice(17, initial_value=False)  # Direction pin 1 for forward
+        self.power_pin_reverse = OutputDevice(27, initial_value=False)  # Direction pin 2 for reverse
+        self.pwm_power = PWMOutputDevice(18, frequency=1000, initial_value=0)  # PWM pin for power control
+        self.pwm_steering = PWMOutputDevice(12)  # PWM pin for steering control
