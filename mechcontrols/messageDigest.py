70b034e8d4439776e049f93e71493b0a289e0bcb
Alan <alyeldinshahin@gmail.com>
2024-08-17 16:52:44 -0400

    tes

--- mechcontrols/messageDigest.py
+++ mechcontrols/messageDigest.py
@@ -6,6 +6,7 @@ power_pin_forward = OutputDevice(17, initial_value=False)  # Direction pin 1 for
 power_pin_reverse = OutputDevice(27, initial_value=False)  # Direction pin 2 for reverse
 pwm_power = PWMOutputDevice(18, frequency=1000, initial_value=0)  # PWM pin for power control
 pwm_steering = PWMOutputDevice(12)  # PWM pin for steering control
+print (power_pin_forward,power_pin_reverse,pwm_power,pwm_steering)
 
 def messageHandler(message):
     try:
